7704340e8288b65498bc8f138e5a1362918b475a
Sanket Kurkute <145901190+sanketkurkute888@users.noreply.github.com>
2024-02-10 12:53:41 +0530

    Add files via upload

---
+++ NumberInWord/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+         NumberInWords numberInWords=new NumberInWords();
+        numberInWords.printNumberInWord(-9);
+
+    }
+}
---
+++ NumberInWord/src/NumberInWords.java
@@ -0,0 +1,54 @@
+public class NumberInWords {
+   public void  printNumberInWord(int number)
+    {
+        switch (number) {
+            case 0 -> System.out.println("zero");
+            case 1 -> System.out.println("one");
+            case 2 -> System.out.println("two");
+            case 3 -> System.out.println("three");
+            case 4 -> System.out.println("four");
+            case 5 -> System.out.println("five");
+            case 6 -> System.out.println("six");
+            case 7 -> System.out.println("seven");
+            case 8 -> System.out.println("eight");
+            case 9 -> System.out.println("nine");
+            default -> System.out.println("Other");
+        }
+//        switch (number)
+//        {
+//            case 0:
+//                System.out.println( "zero");
+//                break;
+//            case 1:
+//                System.out.println("one");
+//                break;
+//            case 2:
+//                System.out.println("two");
+//                break;
+//            case 3:
+//                System.out.println("three");
+//                break;
+//            case 4:
+//                System.out.println("four");
+//                break;
+//            case 5:
+//                System.out.println("five");
+//                break;
+//            case 6:
+//                System.out.println("six");
+//                break;
+//            case 7:
+//                System.out.println("seven");
+//                break;
+//            case 8:
+//                System.out.println("eight");
+//                break;
+//            case 9:
+//                System.out.println("nine");
+//                break;
+//            default:
+//                System.out.println("Other");
+//        }
+    }
+}
+
