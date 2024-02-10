70bd56f0cb40edafdc99498e0ed18a02d2289de6
Sanket Kurkute <145901190+sanketkurkute888@users.noreply.github.com>
2024-02-10 13:10:38 +0530

    Add files via upload

---
+++ NumberToWords/src/NumberToWordsMain.java
@@ -0,0 +1,6 @@
+public class NumberToWordsMain {
+    public static void main(String[] args) {
+        NumberToWords numberToWords=new NumberToWords();
+        numberToWords.numberToWords(-123);
+    }
+}
