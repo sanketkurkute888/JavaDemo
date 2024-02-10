543bbf8eed38b5aa8728efa5c6951ae016ba957d
Sanket Kurkute <145901190+sanketkurkute888@users.noreply.github.com>
2024-02-10 12:30:11 +0530

    Add files via upload

---
+++ FindOccurences/src/EachCharacterFrequencyMain.java
@@ -0,0 +1,9 @@
+public class EachCharacterFrequencyMain {
+    public static void main(String[] args) {
+        EachCharacterFrequency eachCharacterFrequency = new EachCharacterFrequency();
+        eachCharacterFrequency.NumberOfTimesEachChracter("RAMAKANT");
+    }
+
+
+}
+
