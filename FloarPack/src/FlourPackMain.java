16a026beaf562bdbff4a746e7d26b40cd8ed4b5b
Sanket Kurkute <145901190+sanketkurkute888@users.noreply.github.com>
2024-02-10 12:31:08 +0530

    Add files via upload

---
+++ FloarPack/src/FlourPackMain.java
@@ -0,0 +1,6 @@
+public class FlourPackMain {
+    public static void main(String[] args) {
+           FlourPack flourPack=new FlourPack();
+        System.out.println(flourPack.canPack(-3,2,12));
+    }
+}
